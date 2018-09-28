e13c4c56503a231b768aa885d877340a5d3bb4f4
Illyrix <Illyrix@outlook.com>
2018-09-29 00:39:50 +0800

    init another app

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import SalarySheet from './src/SalarySheet'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <SalarySheet></SalarySheet>
+    )
+  }
+}
---
+++ src/SalarySheet.js
@@ -0,0 +1,141 @@
+import React from 'react'
+import {
+  Platform,
+  ToastAndroid,
+  View,
+  Alert,
+  ScrollView
+} from 'react-native'
+import {
+  FormInput,
+  FormLabel,
+  FormValidationMessage,
+  Button,
+  Header,
+  List,
+  ListItem,
+  Text,
+  Divider
+} from 'react-native-elements'
+
+import { find, findAll, save } from './Orm'
+
+const alert = Alert.alert
+
+let uniqueId = 0
+
+let testEmployees = [
+  {name: '11', salary: '1', uniqueId: 1245001},
+  {name: '11', salary: '2', uniqueId: 1245002},
+  {name: '11', salary: '3', uniqueId: 1245003},
+  {name: '11', salary: '4', uniqueId: 1245004},
+  {name: '11', salary: '5', uniqueId: 1245005},
+  {name: '11', salary: '6', uniqueId: 1245006},
+  {name: '11', salary: '7', uniqueId: 1245007},
+  {name: '11', salary: '8', uniqueId: 1245008}
+]
+
+export default class SalarySheet extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      invalidSalary: false,
+      salary: '',
+      name: '',
+      employees: []
+    }
+    this.onChangeSalary = this.onChangeSalary.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  componentDidMount() {
+    findAll().then((res) => {
+      this.setState({ employees: res })
+    })
+  }
+  
+  onChangeSalary(input) {
+    let invalid = false
+    if (input === '') {
+      invalid = false
+    } else {
+      // invalid number format?
+      invalid = isNaN(input) || isNaN(parseFloat(input)) ? true : false
+    }
+    this.setState(() => ({ invalidSalary: invalid, salary: input }))
+  }
+
+  onSubmit() {
+    if (this.state.invalidSalary) {
+      alert('Invalid Salary Input')
+    } else if (this.state.salary === '' || this.state.name === '') {
+      alert('Empty String Input')
+    } else {
+      uniqueId += 1
+      save(uniqueId, this.state.name, this.state.salary).then(() => {
+        this.setState((prev) => {
+          let employees = prev.employees
+          employees.push({ name: this.state.name, salary: this.state.salary, uniqueId })
+          return { employees, name: '', salary: ''}
+        })
+
+        if (Platform.OS === 'android') {
+          ToastAndroid.showWithGravity(
+            'New item has been saved',
+            ToastAndroid.SHORT,
+            ToastAndroid.BOTTOM
+          )
+        }
+      })
+    }
+  }
+
+  render() {
+    const error_msg = this.state.invalidSalary ?
+    <FormValidationMessage >{ 'Only number accepted' }</FormValidationMessage>
+    : null
+
+    return (
+      <View>
+        <Header
+          centerComponent={{ text: 'Awesome Project', style: { color: '#fff', fontSize: 18 } }}
+        />
+        <ScrollView>
+        <View>
+          <FormLabel>Name</FormLabel>
+          <FormInput
+            value={ this.state.name }
+            onChangeText={ name => this.setState({ name }) }/>
+          
+          <FormLabel>Salary</FormLabel>
+          <FormInput
+            value={ this.state.salary }
+            onChangeText={ this.onChangeSalary }
+            keyboardType='numeric'/>
+
+          { error_msg }
+          
+          <Button
+            title='Submit'
+            onPress={ this.onSubmit }
+            rightIcon={{ name: 'done' }}
+            backgroundColor='#09af00'/>
+        </View>
+        <Divider style={{ backgroundColor: '#FFF', height: 40 }} />
+        <Text style={{ color: '#777', marginLeft: 10 }}>{ 'All Employees: ' }</Text>
+        <List>
+          {
+            this.state.employees.map((item)=>(
+              <ListItem
+                title={item.name}
+                subtitle={item.salary}
+                key={item.uniqueId.toString()}
+              />
+            ))
+          }
+        </List>
+        </ScrollView>
+      </View>
+    )
+  }
+}
